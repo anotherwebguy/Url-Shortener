0aaeb43a46cb256a495ba096a1db463a90c30505
Mohit <mohitrajputms123@gmail.com>
2021-07-24 07:11:54 +0530

    flash messages

--- urlshort.py
+++ urlshort.py
@@ -1,8 +1,11 @@
+from os import WIFCONTINUED
 from flask import Flask,render_template,request
-from flask.helpers import url_for
+from flask.helpers import flash, url_for
 from werkzeug.utils import redirect
+import json,os.path
 
 app = Flask(__name__)
+app.secret_key = "vinland"
 
 @app.route('/')
 def index():
@@ -11,6 +14,18 @@ def index():
 @app.route('/your-url',methods=['GET','POST'])
 def your_url():
     if request.method == 'POST':
+       urls={}
+       if os.path.exists('urls.json'):
+           with open('urls.json') as urls_file:
+               urls = json.load(urls_file)
+       if request.form['code'] in urls.keys():
+           flash('This short name has been taken. Please try another')
+           return redirect(url_for('index'))
+
+       urls[request.form['code']] = {'url':request.form['url']}
+       with open('urls.json','w') as url_file:
+           json.dump(urls,url_file)
+
        return render_template('your_url.html',code=request.form['code'])    
     else:
         return redirect(url_for('index'))
