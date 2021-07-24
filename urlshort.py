fa863cb4ace9622f12a84f99371a5481430eed5b
Mohit <mohitrajputms123@gmail.com>
2021-07-24 06:32:26 +0530

    created forms

--- urlshort.py
+++ urlshort.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template,request
+from flask.helpers import url_for
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1> hello world</h1>'
+    return render_template('index.html')
+
+@app.route('/your-url',methods=['GET','POST'])
+def your_url():
+    if request.method == 'POST':
+       return render_template('your_url.html',code=request.form['code'])    
+    else:
+        return redirect(url_for('index'))
